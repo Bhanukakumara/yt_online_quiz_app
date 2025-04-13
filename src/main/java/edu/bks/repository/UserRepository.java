7697ed23e6d8a6e24eb324a2d9979f893bd83212
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-13 14:19:10 +0530

    admin accunt create

--- src/main/java/edu/bks/repository/UserRepository.java
+++ src/main/java/edu/bks/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package edu.bks.repository;
 
 import edu.bks.entity.User;
+import edu.bks.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByRole(UserRole role);
+    User findFirstByEmail(String email);
 }
--- src/main/java/edu/bks/service/user/UserServiceImpl.java
+++ src/main/java/edu/bks/service/user/UserServiceImpl.java
@@ -1,7 +1,29 @@
 package edu.bks.service.user;
 
+import edu.bks.entity.User;
+import edu.bks.enums.UserRole;
+import edu.bks.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    private void createAdminUser(){
+        if (userRepository.findByRole(UserRole.ADMIN) == null){
+            User user = new User();
+            user.setName("Admin");
+            user.setEmail("admin@gmail.com");
+            user.setRole(UserRole.ADMIN);
+            user.setPassword("admin");
+            userRepository.save(user);
+        }
+    }
+    private Boolean hasUserWithEmail(String email){
+        return userRepository.findFirstByEmail(email) != null;
+    }
 }
